93a7be441b6d8fada34a288f1f0b4721d7f6bf45
Valeria Saparina <kzn.2003valeria@gmail.com>
2023-10-26 22:33:40 +0300

    homework 2

---
+++ Homework2/app/src/main/java/com/example/homework2/BaseFragment.kt
@@ -0,0 +1,8 @@
+package com.example.homework2
+
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+open class BaseFragment(@LayoutRes layout: Int) : Fragment(layout) {
+
+}
